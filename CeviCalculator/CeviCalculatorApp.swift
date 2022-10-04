7a5cddfaab85fd966af90150bca1e1aa8c5124b7
gaidaasd00 <swiftgaida@list.ru>
2022-10-04 19:51:45 +0300

    Initial Commit

---
+++ CeviCalculator/CeviCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeviCalculatorApp.swift
+//  CeviCalculator
+//
+//  Created by Алексей Гайдуков on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CeviCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
